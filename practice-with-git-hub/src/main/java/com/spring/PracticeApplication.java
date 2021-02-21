ffb699efa3be543dd208c0cb333503d7747e13f5
Shivakumar <Shivakumar@DESKTOP-9PDBQK6>
2021-02-21 16:27:47 +0530

    Added new names

--- practice-with-git-hub/src/main/java/com/spring/PracticeApplication.java
+++ practice-with-git-hub/src/main/java/com/spring/PracticeApplication.java
@@ -9,12 +9,12 @@ public class PracticeApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(PracticeApplication.class, args);
 		
-		String[] names = {"Akash","Adarsh","Vijaya","Vikas","Niraj"};
+		String[] names = {"Akash","Adarsh","Vijaya","Vikas","Niraj","Vinod","Nayak"};
 		
 		for(String name : names) {
-		
 		System.out.println(name);
 		}
+		System.out.println("hii...");
 	}
 
 }
